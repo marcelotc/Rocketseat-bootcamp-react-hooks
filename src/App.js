3db83a8c64eae31601a1d6e7f9b238e9dfdbddcf
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2019-12-29 22:40:33 -0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [tech, setTech] = useState([
+    'ReactJS',
+    'React Native'
+  ]);
+
+  const [newTech, setNewTech] = useState([]);
+
+  function adicionaTech() {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }
+
   return (
-    <div className="App">
-      <h1>Teste</h1>
-    </div>
+    <>
+      <ul>
+        {tech.map(t =>
+          <li key={t}>{t}</li>
+        )}
+      </ul>
+      <input value={newTech} onChange={e => setNewTech(e.target.value)}></input>
+      <button type="button" onClick={adicionaTech}>Adicionar</button>
+    </>
   );
 }
 
